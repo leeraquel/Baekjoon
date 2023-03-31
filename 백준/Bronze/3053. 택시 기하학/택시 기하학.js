16d1d154e0923d0e23604321c47e401979b9f882
leeraquel <121611906+leeraquel@users.noreply.github.com>
2023-03-31 15:20:52 +0900

    [Bronze III] Title: 택시 기하학, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.js
@@ -0,0 +1,9 @@
+
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+const PI = Math.PI
+const r = parseInt(input);
+
+console.log((PI * r * r).toFixed(6))
+
+console.log((2 * r * r).toFixed(6))
