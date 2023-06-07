80f80c8fa141f7ae34ee0b19b4d62940d101562a
leeraquel <121611906+leeraquel@users.noreply.github.com>
2023-06-07 17:56:26 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        int diff = 2541 - 1998;
+        System.out.println(year - diff);
+     }
+}
