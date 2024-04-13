149a2d387dddd2ffe3f60b86e0f7b2464905179a
leeraquel <121611906+leeraquel@users.noreply.github.com>
2024-04-13 19:24:43 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    var answer = 0;
-    if ( n % 7 === 0 ) {
-        answer = n / 7 ;
-    } 
-     else if ( n / 7 < 1 ) {
-        answer = 1
-        return answer
-    } 
-    else {
-        answer = parseInt(n / 7)+ 1
-    }
-    return answer;
+   return Math.ceil(n / 7)
 }
