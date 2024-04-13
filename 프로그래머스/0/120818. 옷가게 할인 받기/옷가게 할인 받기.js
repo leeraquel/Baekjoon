17f5882b348365c8812c7a2c03efccd694b0c6d2
leeraquel <121611906+leeraquel@users.noreply.github.com>
2024-04-13 19:32:28 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -2,10 +2,10 @@ function solution(price) {
     if (price < 100000) {
         return price
     } else if ( price < 300000) {
-        return parseInt(price * 0.95)
+        return ~~(price * 0.95)
     } else if ( price < 500000) {
-        return parseInt(price * 0.9)
+        return ~~(price * 0.9)
     } else {
-        return parseInt(price * 0.8)
+        return ~~(price * 0.8)
     }
 }
